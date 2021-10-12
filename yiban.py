9cd91f20043c684692030cdc99cc7e63115acd34
simon4862021 <80331096+simon4862021@users.noreply.github.com>
2021-10-13 00:44:23 +0800

    Update yiban.py

--- yiban.py
+++ yiban.py
@@ -52,10 +52,30 @@ class Yiban:
                 print(str(e))
         return response.json()
     
+    def encryptPassword(self, pwd):
+        #密码加密
+        PUBLIC_KEY = '''-----BEGIN PUBLIC KEY-----
+            MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA6aTDM8BhCS8O0wlx2KzA
+            Ajffez4G4A/QSnn1ZDuvLRbKBHm0vVBtBhD03QUnnHXvqigsOOwr4onUeNljegIC
+            XC9h5exLFidQVB58MBjItMA81YVlZKBY9zth1neHeRTWlFTCx+WasvbS0HuYpF8+
+            KPl7LJPjtI4XAAOLBntQGnPwCX2Ff/LgwqkZbOrHHkN444iLmViCXxNUDUMUR9bP
+            A9/I5kwfyZ/mM5m8+IPhSXZ0f2uw1WLov1P4aeKkaaKCf5eL3n7/2vgq7kw2qSmR
+            AGBZzW45PsjOEvygXFOy2n7AXL9nHogDiMdbe4aY2VT70sl0ccc4uvVOvVBMinOp
+            d2rEpX0/8YE0dRXxukrM7i+r6lWy1lSKbP+0tQxQHNa/Cjg5W3uU+W9YmNUFc1w/
+            7QT4SZrnRBEo++Xf9D3YNaOCFZXhy63IpY4eTQCJFQcXdnRbTXEdC3CtWNd7SV/h
+            mfJYekb3GEV+10xLOvpe/+tCTeCDpFDJP6UuzLXBBADL2oV3D56hYlOlscjBokNU
+            AYYlWgfwA91NjDsWW9mwapm/eLs4FNyH0JcMFTWH9dnl8B7PCUra/Lg/IVv6HkFE
+            uCL7hVXGMbw2BZuCIC2VG1ZQ6QD64X8g5zL+HDsusQDbEJV2ZtojalTIjpxMksbR
+            ZRsH+P3+NNOZOEwUdjJUAx8CAwEAAQ==
+            -----END PUBLIC KEY-----'''
+        cipher = PKCS1_v1_5.new(RSA.importKey(PUBLIC_KEY))
+        cipher_text = base64.b64encode(cipher.encrypt(bytes(pwd, encoding="utf8")))
+        return cipher_text.decode("utf-8")
+    
     def login(self):
         params = {
             "mobile": self.mobile,
-            "password": self.password,
+            "password": self.encryptPassword(self.password),
             "ct": "2",
             "identify": "0",
         }
