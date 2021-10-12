b2435f836db8a611985a1f39e4b0a68db7a64e2f
simon4862021 <80331096+simon4862021@users.noreply.github.com>
2021-10-13 01:11:48 +0800

    Update yiban.py

--- yiban.py
+++ yiban.py
@@ -98,16 +98,17 @@ class Yiban:
 #         act = self.session.get("https://f.yiban.cn/iapp/index?act=iapp7463", allow_redirects=False, cookies=self.COOKIES)
         act = self.session.get("https://f.yiban.cn/iapp/index", allow_redirects=False, cookies=self.COOKIES)
         print("act:",act.headers)
-        verifyRequest = re.findall(r"verify_request=(.*?)&",act.headers['Location'])[0]
+#        verifyRequest = re.findall(r"verify_request=(.*?)&",act.headers['Location'])[0]
         self.HEADERS.update({
             'origin': 'https://app.uyiban.com',
             'referer': 'https://app.uyiban.com/',
             'Host': 'api.uyiban.com',
             'user-agent': 'yiban'
         })
-        response = self.request(
-            "https://api.uyiban.com/base/c/auth/yiban?verifyRequest=" + verifyRequest + "&CSRF=" + self.CSRF,
-            cookies=self.COOKIES)
+ #       response = self.request(
+ #           "https://api.uyiban.com/base/c/auth/yiban?verifyRequest=" + verifyRequest + "&CSRF=" + self.CSRF,cookies=self.COOKIES)
+         response = self.request(
+            "https://api.uyiban.com/base/c/auth/yiban" + "?CSRF=" + self.CSRF,cookies=self.COOKIES)
         self.name = response["data"]["PersonName"]
         return response
 
