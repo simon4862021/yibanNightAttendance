979b0d3c5e9330c04440b3e4fc6543e78639842f
simon4862021 <80331096+simon4862021@users.noreply.github.com>
2021-10-14 00:53:49 +0800

    Update yiban.py

--- yiban.py
+++ yiban.py
@@ -154,12 +154,12 @@ def main():
         print(status)
         if status["code"] == 0:
             print("成功!")
-            return 0
+            exit(0)
             #os.environ.setdefault('Status','成功')
         else:
             print("失败!")
             #os.environ.setdefault('Status','失败')
-            return 1
+            exit(1)
     
 if __name__ == '__main__':
      main()
