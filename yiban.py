883b808577f57212c0c2e230b1e887063022bd6c
simon4862021 <80331096+simon4862021@users.noreply.github.com>
2021-11-25 21:07:25 +0800

    Update yiban.py

--- yiban.py
+++ yiban.py
@@ -142,11 +142,8 @@ def main():
     # 修改下方的手机号和密码，即可实现一个宿舍一起签到
     MOBILE=os.environ["MOBILE"]
     PASSWORD=os.environ["PASSWORD"]
-    MOBILE2=os.environ["MOBILE2"]
-    PASSWORD2=os.environ["PASSWORD2"]
     a = Yiban(MOBILE, PASSWORD)
-    b = Yiban(MOBILE2, PASSWORD2)
-    yb_list = [a,b]
+    yb_list = [a]
     for i in range(len(yb_list)):
         status = yb_list[i].setall()
         if status["code"] == 0:
